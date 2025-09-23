78248f2e9eb5622c43b278888a960b7884362033
Narckomanser <67956777+Narckomanser@users.noreply.github.com>
2025-09-23 23:08:35 +0300

    base tcp server

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
+#include <boost/asio.hpp>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+using boost::asio::ip::tcp;
+
+static const unsigned short DEFAULT_PORT = 5555;
+
+int main()
+{
+    try
+    {
+        //TODO should read from property file and check port accessibility before use or DEFAULT_PORT
+        const unsigned short PORT = DEFAULT_PORT;
+
+        boost::asio::io_context io_context;
+        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), PORT));
+
+        std::cout << "[server] listening on " << PORT << std::endl;
+
+        for (;;) {
+            tcp::socket sock(io_context);
+            acceptor.accept(sock);
+            std::cout << "[server] client connected from "
+                      << sock.remote_endpoint().address().to_string() << std::endl;
+            boost::asio::write(sock, boost::asio::buffer("hello from asio\n"));
+        }
+    }
+        catch(const std::exception& e)
+        {
+            std::cerr << "[server] ERROR: " << e.what() << std::endl;
+            return 1;
+        }
+    }
