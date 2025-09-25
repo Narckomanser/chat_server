b9b1ae62f7abbb74a55471f0636f2a571d2c377d
Ilya <>
2025-09-25 17:58:44 +0300

    wip extend server class to work with room logic

--- src/Private/Server.cpp
+++ src/Private/Server.cpp
@@ -1,8 +1,10 @@
 #include "../Public/Server.h"
 
 #include <iostream>
+#include <regex>
 
 #include "../Public/Session.h"
+#include "../Public/Room.h"
 
 using boost::asio::ip::tcp;
 
@@ -18,8 +20,72 @@ void Server::start_accept()
 
 void Server::remove_session(const std::shared_ptr<Session>& session)
 {
-    if (auto it_to_remove = sessions_.find(session); it_to_remove != sessions_.end())
-    sessions_.erase(it_to_remove);
+    sessions_.erase(session);
+    drop_nick(session);
+}
+
+static bool valid_name(const std::string& nick)
+{
+    static const std::regex nick_validator("^a-zA-z0-9_-{2,20}$");
+    return std::regex_match(nick, nick_validator);
+}
+
+bool Server::set_nick(std::shared_ptr<Session>& session, const std::string& new_nick, std::string reason)
+{
+    // check is valid
+    if (!valid_name(new_nick))
+    {
+        reason = "invalid nick";
+        return false;
+    }
+
+    // check is taken
+    if (const auto it_found_nick = nick_registry_.find(new_nick); it_found_nick != nick_registry_.end())
+    {
+        if (const auto alive = it_found_nick->second.lock())
+        {
+            if (alive.get() != session.get())
+            {
+                reason = "nick already taken";
+                return false;
+            }
+            else
+            {
+                nick_registry_.erase(it_found_nick);
+            }
+        }
+    }
+
+    // delete old if exist
+    for (auto it_nick = nick_registry_.begin(); it_nick != nick_registry_.end();)
+    {
+        if (const auto found_session = it_nick->second.lock(); !found_session)
+        {
+            it_nick = nick_registry_.erase(it_nick);
+        }
+        else if (found_session.get() == session.get())
+        {
+            it_nick = nick_registry_.erase(it_nick);
+        }
+        else it_nick++;
+    }
+
+    nick_registry_[new_nick] = session;
+    return true;
+}
+
+void Server::drop_nick(const std::shared_ptr<Session>& session) {}
+std::shared_ptr<Session> Server::find_session_by_nick(const std::string& nick)
+{
+    return std::shared_ptr<Session>();
+}
+std::shared_ptr<Room> Server::get_or_create_room(const std::string& room_name)
+{
+    return std::shared_ptr<Room>();
+}
+std::vector<std::string> Server::get_room_list()
+{
+    return std::vector<std::string>();
 }
 
 void Server::do_accept()
@@ -41,5 +107,4 @@ void Server::on_accept(std::shared_ptr<boost::asio::ip::tcp::socket> socket, con
     auto session = std::make_shared<Session>(std::move(*socket), shared_from_this());
     sessions_.insert(session);
     session->start();
-
 }
--- src/Public/Server.h
+++ src/Public/Server.h
@@ -2,10 +2,14 @@
 
 #include "boost/asio.hpp"
 
-#include "unordered_set"
-#include "memory"
+#include <unordered_set>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <memory>
 
 class Session;
+class Room;
 
 namespace asio = boost::asio;
 using tcp = asio::ip::tcp;
@@ -14,8 +18,16 @@ class Server : public std::enable_shared_from_this<Server>
 {
 public:
     Server(asio::io_context& context, uint16_t port);
+
     void start_accept();
     void remove_session(const std::shared_ptr<Session>& session);
+
+    bool set_nick(std::shared_ptr<Session>& session, const std::string& new_nick, std::string reason);
+    void drop_nick(const std::shared_ptr<Session>& session);
+    std::shared_ptr<Session> find_session_by_nick(const std::string& nick);
+
+    std::shared_ptr<Room> get_or_create_room(const std::string& room_name);
+    std::vector<std::string> get_room_list();
 
 private:
     void do_accept();
@@ -25,4 +37,6 @@ private:
 private:
     tcp::acceptor acceptor_;
     std::unordered_set<std::shared_ptr<Session>> sessions_;
+    std::unordered_map<std::string, std::weak_ptr<Session>>nick_registry_;
+    std::unordered_map<std::string, std::shared_ptr<Room>> rooms_;
 };
